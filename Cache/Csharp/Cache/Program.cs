7b5cc408c7cf0e815b4c24bef7bc38b899b8d2ba
unknown <amit.aditya.1807@gmail.com>
2026-04-26 17:50:28 +0530

    initial commit Cache

---
+++ Cache/Csharp/Cache/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Cache
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cache system design !!");
+
+
+        }
+    }
+}
